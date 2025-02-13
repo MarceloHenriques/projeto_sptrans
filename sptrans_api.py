2eae2d585ba706d088cad6c7ad0f9e692eb325c0
MarceloHenriques <marcelohenriques2003@gmail.com>
2025-02-13 17:34:14 -0300

    Commit Conexão API

--- sptrans_api.py
+++ sptrans_api.py
@@ -5,13 +5,13 @@ import os
 # Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
+api_key = os.getenv('API_KEY')
 
-if not API_KEY:
-    raise ValueError("Chave da API não encontrada. Verifique o arquivo .env.")
+if not api_key:
+    raise ValueError("Chave da API não encontrada.")
 
 url = "http://api.olhovivo.sptrans.com.br/v2.1"
-headers = {"Authorization": f"Bearer {API_KEY}"}
+headers = {"Authorization": f"Bearer {api_key}"}
 response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
